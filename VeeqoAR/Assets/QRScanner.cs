8cd1f05a2c07eaec4d774bcb4b44ee7ec6749e94
Thomas Fisher <thomas.fisher1@ntlworld.com>
2018-03-03 18:41:46 +0000

    QR Code Scanning (Only Independent to AR)
    QR code scanning now works but only independently of using AR features.
    A library has been used. In order to get QR code scanning to occur
    simultanouesly with AR scanning, there must be a common camera interface
    between the two features. Currently, the AR library works using one
    camera setup, but the QR library works with a completely different
    camera setup. If this QR library is to be used in the future, scripts
    must be modified to access the camera in the same way that is required
    by ARToolkit.
    
    Known Problems: Still takes a long time for Android camera to
    initialise.

---
+++ VeeqoAR/Assets/QRScanner.cs
@@ -0,0 +1,117 @@
+﻿using BarcodeScanner;
+using BarcodeScanner.Scanner;
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using Wizcorp.Utils.Logger;
+
+public class QRScanner : MonoBehaviour {
+    private IScanner BarcodeScanner;
+    public Text TextHeader;
+    public RawImage Image;
+    public AudioSource Audio;
+    private float RestartTime;
+
+    // Disable Screen Rotation on that screen
+    void Awake()
+    {
+        Screen.autorotateToPortrait = false;
+        Screen.autorotateToPortraitUpsideDown = false;
+    }
+
+    void Start()
+    {
+        // Create a basic scanner
+        BarcodeScanner = new Scanner();
+        BarcodeScanner.Camera.Play();
+        
+        // Display the camera texture through a RawImage
+        BarcodeScanner.OnReady += (sender, arg) => {
+            // Set Orientation & Texture
+            Image.transform.localEulerAngles = BarcodeScanner.Camera.GetEulerAngles();
+            Image.transform.localScale = BarcodeScanner.Camera.GetScale();
+            Image.texture = BarcodeScanner.Camera.Texture;
+
+            // Keep Image Aspect Ratio
+            var rect = Image.GetComponent<RectTransform>();
+            var newHeight = rect.sizeDelta.x * BarcodeScanner.Camera.Height / BarcodeScanner.Camera.Width;
+            rect.sizeDelta = new Vector2(rect.sizeDelta.x, newHeight);
+
+            RestartTime = Time.realtimeSinceStartup;
+        };
+    }
+
+    /// <summary>
+    /// Start a scan and wait for the callback (wait 1s after a scan success to avoid scanning multiple time the same element)
+    /// </summary>
+    private void StartScanner()
+    {
+        BarcodeScanner.Scan((barCodeType, barCodeValue) => {
+            BarcodeScanner.Stop();
+            if (TextHeader.text.Length > 250)
+            {
+                TextHeader.text = "";
+            }
+            TextHeader.text += "Found: " + barCodeType + " / " + barCodeValue + "\n";
+            RestartTime += Time.realtimeSinceStartup + 1f;
+
+            // Feedback
+            Audio.Play();
+
+#if UNITY_ANDROID || UNITY_IOS
+			Handheld.Vibrate();
+#endif
+        });
+    }
+
+    /// <summary>
+    /// The Update method from unity need to be propagated
+    /// </summary>
+    void Update()
+    {
+        if (BarcodeScanner != null)
+        {
+            BarcodeScanner.Update();
+        }
+
+        // Check if the Scanner need to be started or restarted
+        if (RestartTime != 0 && RestartTime < Time.realtimeSinceStartup)
+        {
+            StartScanner();
+            RestartTime = 0;
+        }
+    }
+
+    #region UI Buttons
+
+    public void ClickBack()
+    {
+        // Try to stop the camera before loading another scene
+        StartCoroutine(StopCamera(() => {
+            SceneManager.LoadScene("Boot");
+        }));
+    }
+
+    /// <summary>
+    /// This coroutine is used because of a bug with unity (http://forum.unity3d.com/threads/closing-scene-with-active-webcamtexture-crashes-on-android-solved.363566/)
+    /// Trying to stop the camera in OnDestroy provoke random crash on Android
+    /// </summary>
+    /// <param name="callback"></param>
+    /// <returns></returns>
+    public IEnumerator StopCamera(Action callback)
+    {
+        // Stop Scanning
+        Image = null;
+        BarcodeScanner.Destroy();
+        BarcodeScanner = null;
+
+        // Wait a bit
+        yield return new WaitForSeconds(0.1f);
+
+        callback.Invoke();
+    }
+
+    #endregion
+}
