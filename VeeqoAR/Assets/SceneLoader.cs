95451da52fe66bb3402bd06265c5fc19631d2485
Thomas Fisher <thomas.fisher1@ntlworld.com>
2018-03-05 13:36:21 +0000

    Main Menu and QR Scanning
    Implemented basic main menu and ability to scan QR codes. Disabled AR
    elements as AR and QR components currently don't work simultaneously.

---
+++ VeeqoAR/Assets/SceneLoader.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+
+    public void LoadScene(int sceneNum)
+    {
+        SceneManager.LoadScene(sceneNum);
+    }
+
+    public void QuitApp()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit ();
+#endif
+    }
+}
