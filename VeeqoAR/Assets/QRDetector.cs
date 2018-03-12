857c2e8f865cf6b6237fa1fc04ae85aee4c72a7e
therealbaylife <benh0795@gmail.com>
2018-03-12 13:05:14 +0000

    QR Detection working with AR Camera
    Added QR Detection script that runs concurrently with ARToolKit - To Use Add QRDetector script to the AR Camera GameObject.

---
+++ VeeqoAR/Assets/QRDetector.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ZXing;
+using ZXing.QrCode;
+
+public class QRDetector : MonoBehaviour {
+    private ARCamera aRcam;
+    private ARTrackedObject arto;
+    private Camera c = null;
+    private Texture2D tex;
+    private string CurrentQR;
+    private int width, height;
+    private BarcodeReader barcodeReader;
+    private RenderTexture rt;
+    private TextMesh text;
+
+    public float GPUTransferRate = 1.0f;
+    private float timeSinceLastGPUTransfer = 0.0f;
+
+    private int W;
+    private int H;
+
+    //Logging
+    private const string LogTag = "QRDetector: ";
+
+    // Use this for initialization
+    IEnumerator Start () {
+        yield return new WaitForEndOfFrame();
+        
+        //Find ARCamera and Camera objects
+        aRcam = FindObjectOfType<ARCamera>(); // (or FindObjectsOfType(typeof(ARCamera)) as ARCamera[])
+        c = aRcam.gameObject.GetComponent<Camera>();
+
+        //Trying to find text overlay to edit based on QR
+        //GameObject textObject = GameObject.Find("Text");
+        //arto = FindObjectOfType<ARTrackedObject>();
+        //text = textObject.gameObject.GetComponent<TextMesh>();
+
+        W = c.pixelWidth;
+        H = c.pixelHeight;
+
+        tex = new Texture2D(W, H, TextureFormat.RGB24, false);
+
+        barcodeReader = new BarcodeReader { AutoRotate = false };
+
+        //Debug.Log(LogTag + "QR Detection Initialised");
+
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        StartCoroutine(DecodeQR());
+	}
+
+    IEnumerator DecodeQR()
+    {
+        //Debug.Log(LogTag + "Start QR Detection");
+        yield return new WaitForEndOfFrame();
+
+        if (timeSinceLastGPUTransfer > 1 / GPUTransferRate)
+        {
+            timeSinceLastGPUTransfer = 0;
+
+            tex.ReadPixels(new Rect(0, 0, W, H), 0, 0);
+            tex.Apply();
+
+            var result = barcodeReader.Decode(tex.GetPixels32(), W, H);
+            if (result != null)
+            {
+                CurrentQR = result.Text;
+                Debug.Log(LogTag + CurrentQR);
+                //text.text = CurrentQR;
+            }
+
+            //Debug.Log(LogTag + "Attempted QR Detection");
+        }
+        else
+        {
+            timeSinceLastGPUTransfer += Time.deltaTime;
+        }
+
+        //Debug.Log(LogTag + "End QR Detection");
+    }
+}
